d1cab55161de744bad47ad6afae5185a4f5d80ab
jonathan-cruz-dev <136759463+jonathan-cruz-dev@users.noreply.github.com>
2025-09-26 09:56:06 -0300

    Objetos Literales

---
+++ semana_08/funciones.js
@@ -0,0 +1,69 @@
+let nombre = "juan";
+let edad = 25;
+let alumno = true;
+
+
+function convertir1( texto){
+    return texto.toUpperCase();
+
+}
+
+const convertir2 = function( texto) {
+    return texto.toUpperCase();
+}
+
+//const convertir3 = ( texto) => { return texto.toUpperCase(); }
+
+const convertir3 =  (texto) => texto.toUpperCase(); 
+
+const formatear = (nombre, fn) => {
+    const formato = "Nombre: " + nombre;
+    return fn( formato);
+}
+
+const lenguajes = ['HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL', 'HTML'];
+// Recorrido de un ARRAY
+/* for(let i=0; i<=4; i++){
+    const lenguaje = lenguajes[i];
+    console.log(lenguaje);
+}
+ */
+
+console.log(typeof(nombre), nombre);
+console.log(typeof(edad), edad);
+console.log(typeof(alumno), alumno);
+console.log(typeof(formatear));
+console.log(typeof(lenguajes));
+
+
+lenguajes.push('Python');
+console.log( lenguajes.length)
+
+lenguajes.forEach(  ( item) => {
+    console.log(item) 
+});
+
+const index = lenguajes.findIndex( (item) => item == 'PHP');
+const elemento = lenguajes.find( (item) => item == 'PHP');
+const encotrados = lenguajes.filter( item => item == 'HTML');
+
+console.log(encotrados)
+// Objeto Literal
+const curso = {
+    // clave : valor
+    materia: 'Programación I',
+    profesor: 'Jonathan',
+    alumnos: ['Ezequiel', 'FRANCO', 'JOAQUIN', 'GUILLERMO', 'MILAGROS', 'SOFIA', 'SELENA' ],
+    aula: 41
+}
+const persona = {
+    "nombre": "Lucas"
+}
+
+// Acceder a datos, con notación punto y notaciór corchetes
+console.log( curso.materia );
+console.log( curso.profesor);
+console.log( curso['aula']);
+
+console.log(curso);
+console.table(persona);
